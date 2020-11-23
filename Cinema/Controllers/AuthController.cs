2fa2b488c25573a7922c0bf19951a1b0d4af08f8
Junior Martinez <jlmjunior@live.com>
2020-11-22 22:43:35 -0300

    Retoramento

--- Cinema/Controllers/AuthController.cs
+++ Cinema/Controllers/AuthController.cs
@@ -1,9 +1,6 @@
 ﻿using Cinema.Data;
 using Cinema.Models;
 using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
