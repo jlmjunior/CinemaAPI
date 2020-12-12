27a2ea97c390bbe46b8b730ac50f7073db5ab278
Junior Martinez <jlmjunior@live.com>
2020-12-12 18:58:35 -0300

    Admin: Usuários e sessões.

---
+++ Cinema/Controllers/AdminController.cs
@@ -0,0 +1,47 @@
+﻿using Cinema.Data;
+using Cinema.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace Cinema.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class AdminController : ApiController
+    {
+        private readonly AdminDAO adminDAO = new AdminDAO();
+
+        [HttpGet]
+        public HttpResponseMessage RetornarUsuarios()
+        {
+            List<UsuarioModel> usuarios = adminDAO.BuscarUsuarios();
+
+            return Request.CreateResponse(HttpStatusCode.OK, new { users = usuarios });
+        }
+
+        [HttpGet]
+        public HttpResponseMessage RetornarSessoes()
+        {
+            DataTable dt = adminDAO.BuscarSessoes();
+            var sessoes = new object();
+
+            if (dt.Rows.Count > 0)
+            {
+                sessoes = dt.AsEnumerable().Select(row => new
+                {
+                    Id = row.Field<int>("id"),
+                    Titulo = row.Field<string>("titulo"),
+                    Horario = row.Field<DateTime>("data_inicio"),
+                    IdSala = row.Field<int>("id_sala")
+                }).ToList();
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, new { sessions = sessoes });
+        }
+    }
+}
---
+++ Cinema/Data/AdminDAO.cs
@@ -0,0 +1,63 @@
+﻿using Cinema.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Cinema.Data
+{
+    public class AdminDAO : DataAccessObject.DAO
+    {
+        public AdminDAO() : base(ConfigurationManager.ConnectionStrings["default"].ConnectionString) { }
+
+        public List<UsuarioModel> BuscarUsuarios()
+        {
+            string query = "SELECT usuario, id_role FROM usuarios";
+
+            SqlCommand cmd = new SqlCommand
+            {
+                CommandType = CommandType.Text,
+                CommandText = query
+            };
+
+            DataTable dt = FillDataTable(cmd);
+
+            if (dt.Rows.Count == 0)
+            {
+                return null;
+            }
+            else
+            {
+                List<UsuarioModel> usuarios = new List<UsuarioModel>();
+
+                foreach (DataRow dr in dt.Rows)
+                {
+                    usuarios.Add(new UsuarioModel
+                    {
+                        Usuario = dr["usuario"].ToString(),
+                        Token = null,
+                        Role = (int)dr["id_role"]
+                    });
+                }
+
+                return usuarios;
+            }
+        }
+
+        public DataTable BuscarSessoes()
+        {
+            string query = "SELECT s.id, f.titulo, s.id_sala, s.data_inicio FROM sessoes s JOIN filmes f ON f.id = s.id_filme";
+
+            SqlCommand cmd = new SqlCommand
+            {
+                CommandType = CommandType.Text,
+                CommandText = query
+            };
+
+            return FillDataTable(cmd);
+        }
+    }
+}
